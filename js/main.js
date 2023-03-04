4c43cbd9a511856d4e0260b456aff6840a2004b0
Hennadii Shevchenko <shege68@gmail.com>
2023-03-04 23:19:42 +0200

    налаштування слайдера(об'єкт, 4 параметра)

--- js/main.js
+++ js/main.js
@@ -1,156 +1,168 @@
-// отримуємо з html id carousel і записуємо в container
-const container = document.querySelector('#carousel');
-// отримуємо слайди
-const slides = container.querySelectorAll('.slide');
-
-// константи
-// кількість слайдів
-const SLIDES_COUNT = slides.length;
-// коди клавіатури
-const CODE_ARROW_LEFT = 'ArrowLeft';
-const CODE_ARROW_RIGHT = 'ArrowRight';
-const CODE_SPACE = 'Space';
-// класи fontawesome
-const FA_PAUSE = '<i class="fas fa-pause-circle"></i >';
-const FA_PLAY = '<i class="fas fa-play-circle"></i >';
-const FA_PREV = '<i class="fas fa-angle-left"></i>';
-const FA_NEXT = '<i class="fas fa-angle-right"></i>';
-
-// змінні
-let currentSlide = 0;
-let timerID = null;
-let isPlaying = true;
-let interval = 2000;
-
-// динамічна ініціалізація кнопок класа controls (порядок важливий !)
-function initControls() {
-  // створюємо елемент div
-  const controls = document.createElement('div');
-  // виводимо кнопки в константи
-  const PAUSE = `<div id="pause-btn" class="control control-pause">${FA_PAUSE}</div>`;
-  const PREV = `<div id="prev-btn" class="control control-prev">${FA_PREV}</div>`;
-  const NEXT = `<div id="next-btn" class="control control-next">${FA_NEXT}</div>`;
-  // добавляємо діву id controls-container та клас controls
-  controls.setAttribute('id', 'controls-container');
-  controls.setAttribute('class', 'controls');
-  // добавляємо в дів кнопки
-  controls.innerHTML = PAUSE + PREV + NEXT;
-  // добавляємо клас controls всередину container
-  container.append(controls);
-  pauseBtn = container.querySelector('#pause-btn');
-  prevBtn = container.querySelector('#prev-btn');
-  nextBtn = container.querySelector('#next-btn');
-}
-
-// динамічна ініціалізація індикаторів класа indicators (порядок важливий !)
-function initIndicators() {
-  // створюємо елемент div
-  const indicators = document.createElement('div');
-
-  indicators.setAttribute('id', 'indicators-container');
-  indicators.setAttribute('class', 'indicators');
-
-  // в циклі добавляємо дочірній клас indicator в indicators по кількості слайдів
-  for (let i = 0; i < SLIDES_COUNT; i++) {
-    const indicator = document.createElement('div');
-    indicator.setAttribute('class', i !== 0 ? 'indicator' : 'indicator active');
-    //indicator.setAttribute('data-slide-to', i);
-    // або через обєкт dataset
-    indicator.dataset.slideTo = `${i}`;
-    indicator.innerHTML = `${i + 1}`;
-    indicators.append(indicator);
+// самовикликаюча функція з 4-ма параметрами
+(function (containerID = '#carousel', slideClass = '.slide', intervalTime, isPlayingFlag) {
+  // обєкт для початкового налаштування слайдера
+  const initConfig = {
+    interval: intervalTime,
+    isPlaying: isPlayingFlag,
+    // отримуємо з html id carousel і записуємо в container
+    container: document.querySelector(containerID),
+    // отримуємо слайди
+    slides: document.querySelectorAll(slideClass)
   }
-  // добавляємо сам клас indicators до батьківського container
-  container.append(indicators);
-  indicatorsContainer = container.querySelector('#indicators-container');
-  indicatorItems = indicatorsContainer.querySelectorAll('.indicator');
-}
 
-function gotoNth(n) {
-  //debugger;
-  console.log(n);
-  slides[currentSlide].classList.toggle('active');
-  indicatorItems[currentSlide].classList.toggle('active');
-  currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
-  indicatorItems[currentSlide].classList.toggle('active');
-  slides[currentSlide].classList.toggle('active');
-}
+  // константи
+  // кількість слайдів
+  const SLIDES_COUNT = initConfig.slides.length;
+  // коди клавіатури
+  const CODE_ARROW_LEFT = 'ArrowLeft';
+  const CODE_ARROW_RIGHT = 'ArrowRight';
+  const CODE_SPACE = 'Space';
+  // класи fontawesome
+  const FA_PAUSE = '<i class="fas fa-pause-circle"></i >';
+  const FA_PLAY = '<i class="fas fa-play-circle"></i >';
+  const FA_PREV = '<i class="fas fa-angle-left"></i>';
+  const FA_NEXT = '<i class="fas fa-angle-right"></i>';
 
-function gotoPrev() {
-  gotoNth(currentSlide - 1);
-}
+  // змінні
+  let currentSlide = 0;
+  let timerID = null;
 
-function gotoNext() {
-  gotoNth(currentSlide + 1);
-}
+  // динамічна ініціалізація кнопок класа controls (порядок важливий !)
+  function initControls() {
+    // створюємо елемент div
+    const controls = document.createElement('div');
+    // виводимо кнопки в константи
+    const PAUSE = `<div id="pause-btn" class="control control-pause">${FA_PAUSE}</div>`;
+    const PREV = `<div id="prev-btn" class="control control-prev">${FA_PREV}</div>`;
+    const NEXT = `<div id="next-btn" class="control control-next">${FA_NEXT}</div>`;
+    // добавляємо діву id controls-container та клас controls
+    controls.setAttribute('id', 'controls-container');
+    controls.setAttribute('class', 'controls');
+    // добавляємо в дів кнопки
+    controls.innerHTML = PAUSE + PREV + NEXT;
+    // добавляємо клас controls всередину container
+    initConfig.container.append(controls);
+    pauseBtn = initConfig.container.querySelector('#pause-btn');
+    prevBtn = initConfig.container.querySelector('#prev-btn');
+    nextBtn = initConfig.container.querySelector('#next-btn');
+  }
 
-function tick() {
-  // функція setInterval спрацьовує циклічно, через 2 сек. і визиває функцію-колбек
-  timerID = setInterval(gotoNext, interval);
-}
+  // динамічна ініціалізація індикаторів класа indicators (порядок важливий !)
+  function initIndicators() {
+    // створюємо елемент div
+    const indicators = document.createElement('div');
 
-function playHandler() {
-  pauseBtn.innerHTML = FA_PAUSE;
-  isPlaying = true;
-  tick();
-}
+    indicators.setAttribute('id', 'indicators-container');
+    indicators.setAttribute('class', 'indicators');
 
-function pauseHandler() {
-  clearInterval(timerID);
-  pauseBtn.innerHTML = FA_PLAY;
-  isPlaying = false;
-}
+    // в циклі добавляємо дочірній клас indicator в indicators по кількості слайдів
+    for (let i = 0; i < SLIDES_COUNT; i++) {
+      const indicator = document.createElement('div');
+      indicator.setAttribute('class', i !== 0 ? 'indicator' : 'indicator active');
+      //indicator.setAttribute('data-slide-to', i);
+      // або через обєкт dataset
+      indicator.dataset.slideTo = `${i}`;
+      indicator.innerHTML = `${i + 1}`;
+      indicators.append(indicator);
+    }
+    // добавляємо сам клас indicators до батьківського container
+    initConfig.container.append(indicators);
+    indicatorsContainer = initConfig.container.querySelector('#indicators-container');
+    indicatorItems = indicatorsContainer.querySelectorAll('.indicator');
+  }
 
-const pausePlay = () => isPlaying ? pauseHandler() : playHandler();
+  function gotoNth(n) {
+    //debugger;
+    console.log(n);
+    initConfig.slides[currentSlide].classList.toggle('active');
+    indicatorItems[currentSlide].classList.toggle('active');
+    currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
+    indicatorItems[currentSlide].classList.toggle('active');
+    initConfig.slides[currentSlide].classList.toggle('active');
+  }
 
-function prev() {
-  pauseHandler();
-  gotoPrev();
-}
+  function gotoPrev() {
+    gotoNth(currentSlide - 1);
+  }
 
-function next() {
-  pauseHandler();
-  gotoNext();
-}
+  function gotoNext() {
+    gotoNth(currentSlide + 1);
+  }
 
-// функція обробник індикаторів
-function indicate(e) {
-  const target = e.target;
+  function tick(flag = true) {
+    if (!flag) {
+      pauseBtn.innerHTML = FA_PLAY;
+      return;
+    }
 
-  if (target && target.classList.contains('indicator')) {
+    if (timerID) return;
+    // функція setInterval спрацьовує циклічно, через заданий інтервал і визиває функцію-колбек
+    timerID = setInterval(gotoNext, initConfig.interval);
+    console.log(timerID);
+  }
+
+  function playHandler() {
+    pauseBtn.innerHTML = FA_PAUSE;
+    initConfig.isPlaying = true;
+    tick();
+  }
+
+  function pauseHandler() {
+    clearInterval(timerID);
+    pauseBtn.innerHTML = FA_PLAY;
+    initConfig.isPlaying = false;
+    timerID = null;
+  }
+
+  const pausePlay = () => initConfig.isPlaying ? pauseHandler() : playHandler();
+
+  function prev() {
     pauseHandler();
-    //gotoNth(+target.getAttribute('data-slide-to'));
-    //console.log(target.getAttribute('data-slide-to'));
-    // або через .dataset і 'data-slide-to' ставиться як .slideTo
-    gotoNth(+target.dataset.slideTo);
+    gotoPrev();
   }
-}
 
-// функція обробник кодів клавіатури
-function pressKey(e) {
-  console.log(e);
-  if (e.code === CODE_ARROW_LEFT) prev();
-  if (e.code === CODE_ARROW_RIGHT) next();
-  if (e.code === CODE_SPACE) pausePlay();
-}
+  function next() {
+    pauseHandler();
+    gotoNext();
+  }
 
-// обробники подій
-function initListener() {
-  pauseBtn.addEventListener('click', pausePlay);
-  prevBtn.addEventListener('click', prev);
-  nextBtn.addEventListener('click', next);
-  indicatorsContainer.addEventListener('click', indicate);
-  // для клавіатури
-  document.addEventListener('keydown', pressKey);
-}
+  // функція обробник індикаторів
+  function indicate(e) {
+    const target = e.target;
 
-// головна функція
-function initApp() {
-  initControls();
-  initIndicators();
-  initListener();
-  tick();
-}
+    if (target && target.classList.contains('indicator')) {
+      pauseHandler();
+      gotoNth(+target.dataset.slideTo);
+    }
+  }
 
-// точка входа !!!
-initApp();
+  // функція обробник кодів клавіатури
+  function pressKey(e) {
+    console.log(e);
+    if (e.code === CODE_ARROW_LEFT) prev();
+    if (e.code === CODE_ARROW_RIGHT) next();
+    if (e.code === CODE_SPACE) pausePlay();
+  }
+
+  // обробники подій
+  function initListener() {
+    pauseBtn.addEventListener('click', pausePlay);
+    prevBtn.addEventListener('click', prev);
+    nextBtn.addEventListener('click', next);
+    indicatorsContainer.addEventListener('click', indicate);
+    // для клавіатури
+    document.addEventListener('keydown', pressKey);
+  }
+
+  // головна функція
+  function initApp() {
+    initControls();
+    initIndicators();
+    initListener();
+    tick(initConfig.isPlaying);
+  }
+
+  // точка входа !!!
+  initApp();
+
+}('#myslider', '.item', 2000, false));
